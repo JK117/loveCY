3076b0135823abebd266177af067dae29f122cab
JK117 <sheppard.jk@outlook.com>
2020-08-30 02:51:22 -0400

    Updates

---
+++ precache-manifest.10250f5f52beca0d23bd5726e08b2b8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7d771fea5c7be13b701c20d568253a",
+    "url": "/loveCY/index.html"
+  },
+  {
+    "revision": "162edd5b31571fd95655",
+    "url": "/loveCY/static/css/main.c696e9f4.chunk.css"
+  },
+  {
+    "revision": "b83938166779b654098b",
+    "url": "/loveCY/static/js/2.8a7b819d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loveCY/static/js/2.8a7b819d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162edd5b31571fd95655",
+    "url": "/loveCY/static/js/main.bf2923b9.chunk.js"
+  },
+  {
+    "revision": "16e0d836517373e7bdf7",
+    "url": "/loveCY/static/js/runtime-main.22233c64.js"
+  },
+  {
+    "revision": "f885a2fe5ecda6b833920e939743fed5",
+    "url": "/loveCY/static/media/Mosaic_01.f885a2fe.jpg"
+  }
+]);
